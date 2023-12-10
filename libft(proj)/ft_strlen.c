3401e10e2188ed721323cccbf480d3e9c66594b8
Bruno Miguel Fernandes Tender <brfernan@c2r9s6.42lisboa.com>
2023-12-10 22:09:22 +0000

    backup

---
+++ libft(proj)/ft_strlen.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 13:30:14 by brfernan          #+#    #+#             */
+/*   Updated: 2023/12/10 18:52:13 by brfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+/*#include <stdio.h>
+int	main(void)
+{
+	printf("%zu\n", ft_strlen("hello"));
+}*/
