5565b25774b9e88d9f1abf03c3c715cb93e5696f
Bruno <bruno@pop-os.localdomain>
2024-01-20 18:17:15 +0000

    backup

--- new_push_swap/main.c
+++ new_push_swap/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 18:37:09 by bruno             #+#    #+#             */
-/*   Updated: 2024/01/19 16:38:58 by brfernan         ###   ########.fr       */
+/*   Updated: 2024/01/20 18:15:12 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ int main(int argc, char* argv[])
 {
 	int	i = 1;
 	t_dlist *aStack = NULL; // change to double pointer according to daan
+	t_dlist *bStack = NULL;
 	if (argc >= 2)
 	{
 		aStack = ft_lstnew(argv[i]);
+		bStack = ft_lstnew(NULL);
 		i++;
 		while (i < argc)
 		{
@@ -39,6 +41,19 @@ int main(int argc, char* argv[])
             t_dlist *to_free = temp;
             temp = temp->next;
             free(to_free);
+			printf("\n\n");
+		}
+		// doesnt work from here on
+		printf("hello");
+		bStack->content = aStack->content;
+		t_dlist *tempo = bStack;
+		while (tempo != NULL)
+		{
+			printf("%s", (char *)tempo->content);
+            t_dlist *to_free = tempo;
+            tempo = tempo->next;
+            free(to_free);
+			printf("\n\n");
 		}
 	}
 }
