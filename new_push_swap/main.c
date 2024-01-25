16251baea7a7e436a528cd7a4bd85e5ee17e76f5
Bruno Miguel Fernandes Tender <brfernan@c2r9s6.42lisboa.com>
2024-01-25 19:13:34 +0000

    backup

--- new_push_swap/main.c
+++ new_push_swap/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 18:37:09 by bruno             #+#    #+#             */
-/*   Updated: 2024/01/23 01:51:14 by bruno            ###   ########.fr       */
+/*   Updated: 2024/01/25 18:06:26 by brfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,63 @@ void	newNode(t_dlist *aStack, char *content)
 	t_dlist *nodetoadd = ft_lstnew(content);
 	ft_lstadd_back(&aStack, nodetoadd);
 }
-void	pushB(t_dlist *aStack, t_dlist **aStackHead, t_dlist *bStack)
+
+void	push(t_dlist **from, t_dlist **to)
 {
-	bStack->content = aStackHead; // ! head of astack doesnt update
+	if (!*from)
+		return;
+	t_dlist *temp = *from;
+	*from = temp->next;
+	temp->next = *to;
+	if (*to)
+		(*to)->prev = temp;
+	temp->prev = NULL;
+	*to = temp;
+}
+
+void	rotateA(t_dlist *aStack, t_dlist **headA)
+{
+	t_dlist *temp = *headA;
+	ft_lstadd_back(&aStack, temp);
+	// ! doesnt work
 }
 
 int main(int argc, char* argv[])
 {
 	int	i = 1;
 	t_dlist *aStack = NULL;
-	t_dlist **aStackHead = &aStack;
+	t_dlist **headA = &aStack;
 	t_dlist *bStack = NULL;
+	t_dlist **headB = &bStack;
 	if (argc >= 2)
 	{
 		aStack = ft_lstnew(argv[i]);
-		bStack = ft_lstnew(NULL);
 		i++;
 		while (i < argc)
 		{
 			newNode(aStack, argv[i]);
 			i++;
 		}
-		t_dlist *temp = aStack; // TODO from here down is testing
-		while (temp != NULL)
-		{
-			printf("%s", (char *)temp->content);
-            t_dlist *to_free = temp;
-            temp = temp->next;
-            free(to_free);
-			printf("\n\n");
-		}
-		// ! doesnt work from here on
-		printf("hello");
-		bStack->content = aStack->content;
-		t_dlist *tempo = bStack;
-		while (tempo != NULL)
-		{
-			printf("%s", (char *)tempo->content);
-            t_dlist *to_free = tempo;
-            tempo = tempo->next;
-            free(to_free);
-			printf("\n\n");
-		}
+	}
+	push(headA, headB); //specify on the prints if it's pushb or pusha
+	rotateA(aStack, headA);
+	// TODO from here down is testing
+	t_dlist *tempA = aStack;
+	printf("Stack a: \n");
+	while (tempA != NULL)
+	{
+		printf("%s", (char *)tempA->content);
+        t_dlist *to_freeA = tempA;
+        tempA = tempA->next;
+        free (to_freeA);
+	}
+	t_dlist *tempB = bStack;
+	printf("\nStack b: \n");
+	while (tempB != NULL)
+	{
+		printf("%s", (char *)tempB->content);
+        t_dlist *to_freeA = tempB;
+        tempB = tempB->next;
+        free(to_freeA);
 	}
 }
