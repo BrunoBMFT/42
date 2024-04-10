089ad065f57b222191097b9f65731a67e57d3aa8
Bruno <bmmc136@gmail.com>
2024-04-10 22:50:54 +0100

    backup

--- so_long/main.c
+++ so_long/main.c
@@ -3,30 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 18:50:02 by brfernan          #+#    #+#             */
-/*   Updated: 2024/04/10 19:38:15 by brfernan         ###   ########.fr       */
+/*   Updated: 2024/04/10 22:49:42 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	clean(t_map map, t_vars vars, bool *crash)
+int	clean(t_map *map, t_vars *vars, bool crash)
 {
-	free_parser(&map);
-	if (vars.win)
-		mlx_destroy_window(vars.mlx, vars.win);
-	if (vars.mlx)
-		mlx_destroy_display(vars.mlx);
-	if (map.map)
-		free_file(map.map);
-	if (map.visited)
-		free_file((char **)map.visited);
-	free(vars.mlx);
-	if (crash)
-		return (1);
-	return (0);
+	free_parser(map);
+//	if (!vars->win)
+//		mlx_destroy_window(vars->mlx, vars->win);
+	/*if (vars->mlx)// ! not sure if right
+		mlx_destroy_display(vars->mlx);*/
+	if (!vars->mlx)
+		free(vars->mlx);
+//	if (!map->map)
+//		free_file(map->map);
+//	if (!map->visited)
+//		free_file((char **)map->visited);
+	if (crash)//no crash
+		return (0);
+	return (1);
 }
 
 int	main(int ac, char **av)
@@ -34,9 +35,9 @@ int	main(int ac, char **av)
 	t_map	map;
 	t_vars	vars;
 
-	mlx_init(vars);
+	vars.mlx = mlx_init(vars);//check alloc
 	map_init(&map);// TODO init vars instead of map and make map already be in vars
 	if (!parser(ac, av, &map))
 		return(ft_printf("failed successfully"), 1);
-	return (ft_printf("e\n"), clean(map, vars, true));
+	return (ft_printf("e\n"), clean(&map, &vars, true));
 }
