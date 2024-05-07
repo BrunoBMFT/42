b2cb57873d1a63b29d1df12ba95b3abb952bd71b
Bruno <bruno@pop-os.localdomain>
2024-05-07 19:55:39 +0100

    backup

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -1,15 +1,63 @@
 #include "../includes/pipex.h"
 // ./pipex infile "cat" "wc" outfile
 // < infile cat | wc > outfile
-// check path
+
+char	*find_path(char **envp, char *com)
+{
+	char *path;
+	char *part;
+	char **paths;
+	int i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		part = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part, com);
+		free (part);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free (path);
+		i++;
+	}
+	i = -1;
+	while (paths[i++])
+		free(paths[i]);
+	free (paths);
+	return (NULL);
+}
+
+void	fail(void)
+{
+	perror("error kek");
+	exit(EXIT_FAILURE);
+}
+
+void	freecoms(char **com)
+{
+	int i = 0;
+	while (com[i])
+	{
+		free(com[i]);
+		i++;
+	}
+}
+
 void    execute(char *arg, char **envp)
 {
-	write(1, "\nin execute\n", 12);
-	char **com = ft_split(arg, ' ');
-//	char *path = find_path();
-	execve("/home/bruno/42/pipex/infile", com, envp);
-	perror("execve");// dont use perror 
-    exit(EXIT_FAILURE); // Make sure to exit the child process if execve fails 
+	char **com;
+	char *path;
+	com = ft_split(arg, ' ');
+	path = find_path(envp, com[0]);
+	if (!path)
+	{
+		freecoms(com);
+		fail();
+	}
+	if (execve(path, com, envp) == -1)
+		fail();
 }
 
 void	child_process(int *fd, char **av, char **envp)
@@ -18,9 +66,8 @@ void	child_process(int *fd, char **av, char **envp)
 	if (filein == -1)
 		printf("error");
 	close(fd[0]);//close unused path
-//	dup2(fd[1], 1);//do i really need to dup?
-//	dup2(filein, 0);
-	printf("in child\n");//printf dont work if dup2 comes before because stdout becomes fd[1]
+	dup2(fd[1], 1);//do i really need to dup?
+	dup2(filein, 0);
 	execute(av[2], envp);
 	close(fd[1]);
 	exit(EXIT_SUCCESS);//dont know if correct
@@ -29,41 +76,22 @@ void	child_process(int *fd, char **av, char **envp)
 void	parent_process(int *fd, char **av, char **envp)
 {
 	int	fileout = open(av[4], O_WRONLY, 0777);
+	int	extra = open("/home/bruno/42/pipex/extra", O_WRONLY, 0777);
 	if (fileout == -1)
 		printf("error");
 	close(fd[1]);//close unused path
-//	dup2(fd[0], 0);
-//	dup2(fileout, 1);
-	printf("parent\n");
+	dup2(fd[0], 0);//
+	dup2(fileout, 1);
 	wait(NULL);
 	execute(av[3], envp);
+	write(extra, "2", 2);
 	close(fd[0]);
 }
 
-char	*find_path(char **envp)
-{
-	char *path;
-	int i = 0;
-	while (envp[i])
-	{
-		if (envp[i][0] == 'P' && envp[i][1] == 'W')
-			
-		i++;
-	}
-//	return (path);
-	return (NULL);
-}
 
 int main(int ac, char **av, char **envp)
 {
-	/*int  i = 0;
-	while (envp[i])
-	{
-		printf("%s\n", envp[i]);
-		i++;
-	}*/
-	printf("%s\n", find_path(envp));
-	/*int fd[2];
+	int fd[2];
 	pid_t pid;//process id
 	if (ac == 5)
 	{
@@ -79,6 +107,5 @@ int main(int ac, char **av, char **envp)
 			parent_process(fd, av, envp);
 		}
 		return (0);
-	}*/
-
+	}
 }
