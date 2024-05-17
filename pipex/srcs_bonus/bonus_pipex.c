ea89dcf038295f5e148ece1834b7f34e023506fa
bruno <bmmc136@gmail.com>
2024-05-17 15:42:15 +0100

    backup

--- pipex/srcs_bonus/bonus_pipex.c
+++ pipex/srcs_bonus/bonus_pipex.c
@@ -6,7 +6,7 @@
 /*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 14:59:37 by brfernan          #+#    #+#             */
-/*   Updated: 2024/05/17 02:52:28 by bruno            ###   ########.fr       */
+/*   Updated: 2024/05/17 15:41:52 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@
 
 // ./pipex here_doc LIMITER cmd cmd1 file
 // cmd << LIMITER | cmd1 >> file
-
+// TODO error check
 int	execute(char *arg, char **envp)
 {
 	char	**com;
 	char	*path;
 
 	com = ft_split(arg, ' ');
-	ft_putendl(com[0]);
 	path = find_path(envp, com[0]);
 	if (!path)
 	{
@@ -53,27 +52,21 @@ void	child_process(char *av, char **envp)
 		exit(0);
 	}
 	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
 	waitpid(pid, NULL, 0);
+	return ;
 }
 
 int	last_process(char *av, char **envp)
 {
 	int		status;
-	int		fd[2];
 	pid_t	pid;
 
 	status = 0;
-	if (pipe(fd) == -1)
-		error("pipe failed", 1);
 	pid = fork();
 	if (pid == 0)
-	{
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
 		execute(av, envp);
-		exit(0);
-	}
 	waitpid(pid, &status, 0);
 	return (WEXITSTATUS(status));
 }
@@ -94,10 +87,6 @@ int	open_file(char *argv, int i)
 	return (file);
 }
 
-//change dup2 of child to main
-//change dup2 of last to main
-//change forks to process functions
-
 int	main(int ac, char **av, char **envp)
 {
 	int status = 0;
@@ -108,15 +97,15 @@ int	main(int ac, char **av, char **envp)
 	else
 	{
 		int i = 2;
+		int fileout = open_file(av[ac - 1], 1);
 		int filein = open_file(av[1], 2);
 		dup2(filein, STDIN_FILENO);
 		close (filein);
 		while (i < ac - 2)
 			child_process(av[i++], envp);
-		int fileout = open_file(av[ac - 1], 1);
 		dup2(fileout, STDOUT_FILENO);
 		close(fileout);
-		status = last_process(av[i], envp);//check if argv is correct
+		status = last_process(av[ac - 2], envp);//check if argv is correct
 	}
 	return (WEXITSTATUS(status));
 }
