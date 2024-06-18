84f3bb9e33f0f326311fc20c95a5ed9c7f294864
Bruno Miguel Fernandes Tender <brfernan@c1r20s5.42lisboa.com>
2024-06-18 18:57:34 +0100

    backup

--- Minishell/srcs/main.c
+++ Minishell/srcs/main.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 20:13:16 by bruno             #+#    #+#             */
-/*   Updated: 2024/06/17 21:40:18 by bruno            ###   ########.fr       */
+/*   Updated: 2024/06/18 17:09:07 by brfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	caught_echo(char *input, char **envp)
+{
+	
+}
 
 int	main(int ac, char **av, char **envp)
 {
