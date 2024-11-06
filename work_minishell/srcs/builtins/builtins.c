c738aa76d12310b027a50008b2605a566c926305
Bruno <bmmc136@gmail.com>
2024-11-06 16:01:08 +0000

    backup

--- work_minishell/srcs/builtins/builtins.c
+++ work_minishell/srcs/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 18:15:45 by bruno             #+#    #+#             */
-/*   Updated: 2024/10/24 18:44:06 by bruno            ###   ########.fr       */
+/*   Updated: 2024/10/25 16:03:26 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,9 @@ int	try_builtins(t_jobs *job, t_env *env, bool pipe)
 	else if (ft_strcmp(job->job[0], "exit") == 0)
 		status = caught_exit(job, env, pipe);
 	if (pipe && status != 200)
+	{
+		print_jobs("cleaning", job);
 		clean_exit(job, env, status);
+	}
 	return (status);
 }
