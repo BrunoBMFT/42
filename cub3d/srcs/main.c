faf508b9fd4a40caa4e6c5415b627a0d287244a6
Bruno <61157658+BrunoBMFT@users.noreply.github.com>
2025-01-03 03:40:17 +0000

    Add files via upload

--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 00:51:28 by bruno             #+#    #+#             */
-/*   Updated: 2025/01/02 15:52:13 by bruno            ###   ########.fr       */
+/*   Updated: 2025/01/03 03:37:04 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,23 @@ void	init_minimap(t_data *data, int x, int y)
 	data->minimap = malloc(sizeof(t_img));//ugly code from here
 	if (!data->minimap)
 		error(data, "Minimap image allocation failed");
-	data->minimap->width = x / 4;
-	data->minimap->height = y / 4;
+	//TODO find better logic
+	//knowing max length of line for calc
+	int max_len = 0;
+	int i = 0;
+	while (data->map[i])
+	{
+		int temp = ft_strlen(data->map[i]);
+		if (max_len < temp)
+			max_len = temp;
+		i++;
+	}
+	//knowing max length of line for calc
+	//TODO find better logic
+	//make so the size of the actual image is the same?
+	//SIZE OF THE IMG ISNT CORRECT, STUFF AT ALMOST THE END IS NOT SAVED IN THE IMG ITSELF
+	data->minimap->width = max_len * 30;//fix size to map size, for now like this to test
+	data->minimap->height = ft_split_wordcount(data->map) * 30;//fix size to map size, for now like this to test
 	data->minimap->img = mlx_new_image(data->mlx, data->minimap->width, data->minimap->height);
 	if (!data->minimap->img)
 		error(data, "Minimap image failed");
@@ -98,27 +113,13 @@ void	make_frame(t_data *data, t_img *img, int y, int x)
 
 void	make_minimap_pixel(t_data *data, int y, int x, int color)
 {
-	//TODO find better logic
-	//knowing max length of line for calc
-	int max_len = 0;
-	int i = 0;
-	while (data->map[i])
-	{
-		int temp = ft_strlen(data->map[i]);
-		if (max_len < temp)
-			max_len = temp;
-		i++;
-	}
-
-	int mult_y = data->minimap->height / ft_split_wordcount(data->map);
-	int mult_x = data->minimap->width / max_len;
 
 	// put_pixel(data->minimap, y, x, color);
-	i = 0;
-	while (i < 10)//ALL WRONG, but like it was before, the above snippet works
+	int i = 0;
+	while (j < data->minimap->width - 20)//lags quite a bit
 	{
 		int j = 0;
-		while (j < 10)
+		while (j < data->minimap->width - 20)//lags quite a bit
 		{
 			put_pixel(data->minimap, i + y, j + x, color);
 			j++;
@@ -151,32 +152,52 @@ int	main(int ac, char **av)
 //  easier management of minimap if i can put everything inside minimap img
 //	and then just scale it for what i need
 
-//this saves into minimap img
+
 	y = 0;
 	while (data.map[y])
 	{
 		int x = 0;
 		while (data.map[y][x])
 		{
-			make_minimap_pixel(&data, y, x, 111111);
-			// if (data.map[y][x] == '1')
-			// 	make_minimap_pixel(&data, y, x, 111111);//green
-			// if (data.map[y][x] == '0')
-			// 	make_minimap_pixel(&data, y, x, 1111);//dark blue
-			// else if (data.map[y][x] == ' ')
-			// 	make_minimap_pixel(&data, y, x, 11111111);//pink
-			// else if (data.map[y][x])
-			// 	make_minimap_pixel(&data, y, x, 110011);//light blue
+			// if (data.map[y][x] != ' ')
+			// 	make_minimap_pixel(&data, y, x, 111111);
+			if (data.map[y][x] == '1')
+				make_minimap_pixel(&data, y, x, 111111);//green
+			if (data.map[y][x] == '0')
+				make_minimap_pixel(&data, y, x, 1111);//dark blue
+			else
+				make_minimap_pixel(&data, y, x, 11111111);//pink
 			x++;
 		}
 		y++;
 	}
 
+
+
+//this saves into minimap img
+	// y = 0;
+	// while (data.map[y])
+	// {
+	// 	int x = 0;
+	// 	while (data.map[y][x])
+	// 	{
+	// 		// if (data.map[y][x] != ' ')
+	// 		// 	make_minimap_pixel(&data, y, x, 111111);
+	// 		if (data.map[y][x] == '1')
+	// 			make_minimap_pixel(&data, y, x, 111111);//green
+	// 		if (data.map[y][x] == '0')
+	// 			make_minimap_pixel(&data, y, x, 1111);//dark blue
+	// 		else
+	// 			make_minimap_pixel(&data, y, x, 11111111);//pink
+	// 		x++;
+	// 	}
+	// 	y++;
+	// }
+
 	make_frame(&data, data.minimap, 0, 0);
 	
 	
-	
-//everything is tripping, have to normalise a screen size, possible will hard code it to a specific resolution
+
 
 
 	
