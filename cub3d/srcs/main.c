e97b30edc50669586e2326a38d87382491bc8b92
Bruno <bmmc136@gmail.com>
2025-03-21 21:15:54 +0000

    backup

--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 00:51:28 by bruno             #+#    #+#             */
-/*   Updated: 2025/03/20 20:54:50 by bruno            ###   ########.fr       */
+/*   Updated: 2025/03/21 19:37:10 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,54 +50,62 @@ float	rad(int deg)
 	return (deg * PI / 180);
 }
 
-void	draw_player(t_data *data, int d_y, int d_x)
+void	draw_ray(t_data *data, int d_y, int d_x, int angle)
 {
 	d_y = d_y * SCALE;
 	d_x = d_x * SCALE;
-	//pixel
-	put_pixel(&data->frame, d_y, d_x, GREEN);
 
-	//draw a line
-	//while loop starting where player is, and runs until y has changed by 1
-	int angle = 90;//
 	float f_y = (float)d_y;
 	float f_x = (float)d_x;
-	printf("y: %.2f\n", f_y);
-	printf("x: %.2f\n", f_x);
 
-	float desty;
-	float destx;
-	if (angle > 90 && angle < 180)//more checks xD
+	//decide direction
+	int y_dir;
+	int x_dir;
+	//dont forget loops or negatives
+	if (angle >= 0 && angle <= 90)
 	{
-		desty = f_y - 1 * SCALE;// this is + 30
-		printf("dy: %.4f\n", desty);
-		destx = f_y + 1 * SCALE / tan(rad(angle));
-		printf("dx: %.4f\n", destx);
+		y_dir = -1;
+		x_dir = 1;
 	}
-	while (f_y > desty)//find another way for this loop
+	if (angle > 90 && angle <= 180)
 	{
-		// put_pixel(&data->frame, (int)f_y, (int)f_x, GREEN);
-		f_y -= 1;
-		f_x += 1 / tan(rad(angle));
+		y_dir = 1;
+		x_dir = -1;
 	}
-
+	if (angle > 180 && angle <= 270)
+	{
+		y_dir = 1;
+		x_dir = -1;
+	}
+	if (angle > 270 && angle <= 360)
+	{
+		y_dir = -1;
+		x_dir = 1;
+	}
 	
-	// angle = 20;
+	//draw ray
+	int y = (int)round(f_y) / SCALE;
+	int x = (int)round(f_x) / SCALE;
+	while (data->map[y][x] != '1')//this loop will end at map[y][x] == 1
+	{
+		y = (int)round(f_y) / SCALE;
+		x = (int)round(f_x) / SCALE;
+		put_pixel(&data->frame, (int)f_y, (int)f_x, GREEN);
+		f_y += 1 * y_dir;
+		f_x += 1 * tan(rad(angle)) * x_dir;//tan 90 is infinite
+	}
+
+
 	// f_y = (float)d_y;
 	// f_x = (float)d_x;
-	// printf("y: %.2f\n", f_y);
-	// printf("x: %.2f\n", f_x);
-	// desty = f_y + 1 * SCALE;// this is + 30
-	// printf("dy: %.4f\n", desty);
-	// destx = f_y + 1 * SCALE / tan(rad(angle));
-	// printf("dx: %.4f\n", destx);
-	// while (f_y < desty)
+	// while (data->map[y][x] != '1')//this loop will end at map[y][x] == 1
 	// {
+	// 	y = (int)round(f_y) / SCALE;
+	// 	x = (int)round(f_x) / SCALE;
 	// 	put_pixel(&data->frame, (int)f_y, (int)f_x, GREEN);
-	// 	f_y += 1;
-	// 	f_x += 1  / tan(rad(angle));
+	// 	f_x += 1 * x_dir;
+	// 	f_y += 1 / tan(rad(angle)) * y_dir;
 	// }
-	
 }
 
 void	create_map(t_data *data)
@@ -113,8 +121,17 @@ void	create_map(t_data *data)
 			else if (data->map[y][x] == '0')
 				loop_map(data, y, x, GREY);
 			else if (ft_strchr("NESW", data->map[y][x]))
+			{
 				put_pixel(&data->frame, y * SCALE, x * SCALE, GREEN);
-				// draw_player(data, y, x);
+				draw_ray(data, y, x, 87);
+				// int i = 0;
+				// while (i < 360)
+				// {
+				// 	if (i % 90)
+				// 		draw_ray(data, y, x, i);
+				// 	i++;
+				// }
+			}
 			x++;
 		}
 		y++;
