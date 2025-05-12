2d6cdaafb310dbb16afae1a37aa9a406062fb865
Bruno Miguel Fernandes Tender <brfernan@c1r6s6.42porto.com>
2025-05-12 18:39:15 +0100

    backup

---
+++ cub3D/includes/libft/srcs/mall/ft_strjoin.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 14:19:13 by brfernan          #+#    #+#             */
+/*   Updated: 2024/12/16 04:07:26 by bruno            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	if (!s1 && !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin3(char const *s1, char const *s2, char const *s3)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*full;
+
+	i = -1;
+	j = 0;
+	k = 0;
+	full = ft_calloc((ft_strlen(s1)
+				+ ft_strlen(s2) + ft_strlen(s3)) + 1, sizeof(char));
+	if (full == NULL)
+		return (NULL);
+	while (s1 && s1[++i])
+		full[i] = s1[i];
+	while (s2 && s2[j])
+		full[i++] = s2[j++];
+	while (s3 && s3[k])
+		full[i++] = s3[k++];
+	full[i] = '\0';
+	return (full);
+}
---
+++ cub3D/includes/libft/srcs/mall/ft_strtrim.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 22:32:33 by brfernan          #+#    #+#             */
+/*   Updated: 2025/04/10 01:25:24 by brfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*ret;
+	int		i;
+	int		j;
+
+	if (!s1 || !set)
+		return (NULL);
+	ret = 0;
+	i = 0;
+	j = ft_strlen(s1);
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	while (s1[j - 1] && ft_strchr(set, s1[j - 1]) && j > i)
+		j--;
+	ret = (char *)malloc(sizeof(char) * (j - i + 1));
+	if (!ret)
+		return (NULL);
+	ft_strlcpy(ret, &s1[i], j - i + 1);
+	return (ret);
+}
