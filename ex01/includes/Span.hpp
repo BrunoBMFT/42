df67ad046123b417af8a150ad0b4615f4eb0e948
Bruno <61157658+BrunoBMFT@users.noreply.github.com>
2025-09-07 02:22:48 +0100

    a

---
+++ ex01/includes/Span.hpp
@@ -0,0 +1,41 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+# include <iostream>
+# include <vector>
+# include <algorithm>
+
+class Span
+{
+	private:
+		unsigned int		maxSize;
+		std::vector<int>	vect;
+	public:
+		Span();
+		Span(unsigned int n);
+		Span(const Span &oth);
+		Span &operator=(const Span &oth);
+		~Span();
+
+		
+		void addNumber(int num);
+		void addNumber(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+		int shortestSpan();
+		int longestSpan();
+
+		//TODO remove
+		void print();
+
+	class FullContainer : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+
+	class NotEnoughNumbers : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+};
+
+#endif
