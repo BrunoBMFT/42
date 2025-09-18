f45f12e78df8b75dbbff71f091eca24bf053b555
Bruno Miguel Fernandes Tender <brfernan@c2r2s4.42porto.com>
2025-09-18 19:53:10 +0100

    backup

--- MAKING_09/ex00/srcs/main.cpp
+++ MAKING_09/ex00/srcs/main.cpp
@@ -1,18 +1,77 @@
 #include "../includes/BitcoinExchange.hpp"
 #include <fstream>
+#include <vector>
+#include <stdlib.h>//for atof
+//figure out input.txt date|value line
 
-
-int main()
+std::vector<std::string> saveFile(std::string infile)
 {
-	std::ifstream file("../data.csv");//in relation to compilation
-	if (!file) {
-		std::cout << "file not opened\n";
-		return 1;
-	}
+	std::vector<std::string> vec;
+	std::ifstream file(infile.c_str());//in relation to compilation
+	if (!file)
+		throw std::runtime_error("file failed to open\n");
 	std::string line;
 	while (std::getline(file, line))
-	{
-		std::cout << line << std::endl;
+		vec.push_back(line);
+	// for (size_t i = 0; i < vec.size(); i++)//testing
+	// 	std::cout << vec[i] << std::endl;
+	return (vec);
+}
+
+void	processDate(std::string input, std::vector<std::string> db)
+{	
+	std::cout << "input: " << input << std::endl;
+	std::string date = input.substr(0, 10);//does this allocate
+	// std::cout << "date: " << date << std::endl;
+	//todo parse the date
+
+	//*find equivalent date (use lower)
+	std::string temp;//get the value from here
+	for (size_t i = 0; i < db.size(); i++) {
+		if (date > db[i].substr(0, 10))
+			temp = db[i];//why does it not work with break???
 	}
+	std::cout << "in db: "<< temp << std::endl;
+
+	//*calculate info
+	//use find() to get the comma, then save everything else
+	float inputNum = static_cast<float>(atof(input.substr(12, input.length()).c_str()));
+	float dbNum = static_cast<float>(atof(temp.substr(11, temp.length()).c_str()));
+
+	// std::cout << "inputNum: "<< inputNum << std::endl;
+	float final = inputNum * dbNum;
+	//*print info
+	std::cout << input << " = " << final << std::endl;
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2) {
+		std::cout << "Invalid number of arguments" << std::endl;
+		return 1;
+	}
+	
+	//*save db
+	std::vector<std::string> db;
+	try { //stupid shit
+		db = saveFile("data.csv");
+	} catch (std::exception &e) {
+		std::cerr << e.what();
+		return 1;
+	}
+
+	//*save input
+	std::vector<std::string> input;
+	try {//more stupid shit
+		input = saveFile(av[1]);
+	} catch (std::exception &e) {
+		std::cerr << e.what();
+		return 1;
+	}
+
+	processDate(input[1], db);
+	//*loop through input
+	// for (size_t i = 0; i < input.size(); i++)
+	// 	processDate(input[i], db);
 	return 0;
 }
