e51ba4e5f0d1940db60393b5505c11ae2bea0812
Bruno <bmmc136@gmail.com>
2025-09-19 03:27:00 +0100

    backup

--- MAKING_09/ex00/srcs/main.cpp
+++ MAKING_09/ex00/srcs/main.cpp
@@ -1,46 +1,66 @@
 #include "../includes/BitcoinExchange.hpp"
 #include <fstream>
-#include <vector>
+#include <map>
 #include <stdlib.h>//for atof
-//figure out input.txt date|value line
 
-std::vector<std::string> saveFile(std::string infile)
+//todo make these modular
+std::map<std::string, double> saveDb()
 {
-	std::vector<std::string> vec;
-	std::ifstream file(infile.c_str());//in relation to compilation
-	if (!file)
+	std::ifstream file("data.csv");//use .is_open() to check
+	if (!file.is_open())
 		throw std::runtime_error("file failed to open\n");
+	std::map<std::string, double> db;
+
 	std::string line;
-	while (std::getline(file, line))
-		vec.push_back(line);
-	// for (size_t i = 0; i < vec.size(); i++)//testing
-	// 	std::cout << vec[i] << std::endl;
-	return (vec);
-}
-
-void	processDate(std::string input, std::vector<std::string> db)
-{	
-	// std::cout << "input: " << input << std::endl;
-	std::string date = input.substr(0, 10);//does this allocate
-	//todo parse the date
-
-	std::string temp;
-	for (size_t i = 0; i < db.size(); i++) {
-		if (date > db[i].substr(0, 10))
-			temp = db[i];//why does it not work with break???
+	getline(file, line);//skip, dont forget to parse this
+	while (getline(file, line))
+	{
+		std::string::size_type sep = line.find(",");//eww
+		std::string date = line.substr(0, sep);
+		std::string value = line.substr(sep + 1);//wtf
+		db[date] = atof(value.c_str());//shit
 	}
-	// std::cout << "in db: "<< temp << std::endl;
 
-	//*calculate info
-	//! USE FIND INSTEAD OF EVERYTHING ELSE
-	float inputNum = static_cast<float>(atof(input.substr(12, input.length()).c_str()));
-	float dbNum = static_cast<float>(atof(temp.substr(11, temp.length()).c_str()));
+	//*testing
+	// for (std::map<std::string, double>::iterator it = db.begin(); it != db.end(); ++it)
+	// 	std::cout << "date: " << it->first << " value: " << it->second << std::endl;
 
-	// std::cout << "inputNum: "<< inputNum << std::endl;
-	float final = inputNum * dbNum;
-	//*print info
-	std::cout << input << " = " << final << std::endl;
+	return (db);//this wont have to return a map, it will be in class
 }
+
+//technically this doesnt need to be a map
+// i need to save it somehow without using more containers
+// i could do it line by line using getline to be simpler, since i dont exactly need it saved
+
+
+// i have the db, i could justse getline, compare dates, calculate value, print it
+
+//TODO MAP HAS LOWER_BOUND() TO FIND THE CLOSEST DATE!!!
+
+
+void	processData(std::map<std::string, double> db, char **av)
+{
+	std::ifstream file(av[1]);
+	if (!file.is_open())//THIS ISNT CHECKING ANYTHING
+		throw std::runtime_error("file failed to open\n");
+
+	
+	std::string line;
+	getline(file, line);//*parse
+
+
+	while (getline(file, line))//parse input
+	{
+		// std::cout << "input: " << line << std::endl;
+		std::string date = line.substr(0, 10);
+		std::cout << "date: " << date << std::endl;
+		std::map<std::string, double>::iterator pos = db.lower_bound(date);//doesnt work how i wanted it
+		std::cout << "data in db: " << pos->first << std::endl;
+	}
+
+	
+}
+
 
 int main(int ac, char **av)
 {
@@ -48,28 +68,21 @@ int main(int ac, char **av)
 		std::cout << "Invalid number of arguments" << std::endl;
 		return 1;
 	}
-	
 	//*save db
-	std::vector<std::string> db;
+	std::map<std::string, double> db;
 	try { //stupid shit
-		db = saveFile("data.csv");
+		db = saveDb();
+		
+		processData(db, av);
 	} catch (std::exception &e) {
 		std::cerr << e.what();
 		return 1;
 	}
 
-	//*save input
-	std::vector<std::string> input;
-	try {//more stupid shit
-		input = saveFile(av[1]);
-	} catch (std::exception &e) {
-		std::cerr << e.what();
-		return 1;
-	}
 
-	// processDate(input[1], db);
-	//*loop through input
-	for (size_t i = 0; i < input.size(); i++)
-		processDate(input[i], db);
+
+
+	// BitcoinExchange exchange;
+	// exchange.execute(argv[1]);
 	return 0;
 }
