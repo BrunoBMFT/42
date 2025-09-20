12a1422269c7a6bacb4a747fe164c6d78f3166c6
BrunoBMFT <bmmc136@gmail.com>
2025-09-20 14:37:40 +0100

    backup

--- MAKING_09/ex00/srcs/main.cpp
+++ MAKING_09/ex00/srcs/main.cpp
@@ -2,17 +2,23 @@
 #include <fstream>
 #include <map>
 #include <stdlib.h>//for atof
+//todo create exceptions for everything
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
 
-//todo make these modular
 std::map<std::string, double> saveDb()
 {
-	std::ifstream file("data.csv");//use .is_open() to check
+	std::ifstream file("data.csv");
 	if (!file.is_open())
-		throw std::runtime_error("file failed to open\n");
+		throw std::runtime_error("Error: could not open file.\n");
 	std::map<std::string, double> db;
 
 	std::string line;
-	getline(file, line);//skip, dont forget to parse this
+	getline(file, line);//todo skip, dont forget to parse this
 	while (getline(file, line))
 	{
 		std::string::size_type sep = line.find(",");//eww
@@ -21,70 +27,91 @@ std::map<std::string, double> saveDb()
 		db[date] = atof(value.c_str());//shit
 	}
 
-	//*testing
-	// for (std::map<std::string, double>::iterator it = db.begin(); it != db.end(); ++it)
-	// 	std::cout << "date: " << it->first << " value: " << it->second << std::endl;
-
-	return (db);//this wont have to return a map, it will be in class
+	return (db);//todo this wont have to return a map, it will be in class
 }
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
 
-//technically this doesnt need to be a map
-// i need to save it somehow without using more containers
-// i could do it line by line using getline to be simpler, since i dont exactly need it saved
-
-
-// i have the db, i could justse getline, compare dates, calculate value, print it
-
-//TODO MAP HAS LOWER_BOUND() TO FIND THE CLOSEST DATE!!!
-
+bool	processLine(std::map<std::string, double> db, std::string input)
+{
+	if (input.length() < 12){//stupid fix
+		std::cout << "Error: bad input => " << input << std::endl;
+		return (false);
+	}
+	std::string date = input.substr(0, 10);
+	double num = atof(input.substr(12).c_str());
+	if (num < 0) {
+		std::cout << "Error: not a positive number." << std::endl;
+		return (false);
+	}
+	else if (num > 1000){
+		std::cout << "Error: too large a number." << std::endl;
+		return (false);
+	}
+	//todo use auto?
+	//todo parse dates using strptime
+	std::map<std::string, double>::iterator pos = db.lower_bound(date);
+	if (pos->first != date && pos != db.begin())//this should be safe enough, right?
+		--pos;
+	
+	//*calculate (make func for this)
+	double calcValue = pos->second * num;
+	std::cout << date << " => " << num << " = " << calcValue << std::endl;
+	return (true);
+}
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
 
 void	processData(std::map<std::string, double> db, char **av)
 {
 	std::ifstream file(av[1]);
-	if (!file.is_open())//THIS ISNT CHECKING ANYTHING
-		throw std::runtime_error("file failed to open\n");
+	if (!file.is_open())
+		throw std::runtime_error("Error: could not open file.\n");
 
-	
-	std::string line;
-	getline(file, line);//*parse
+	std::string input;
+	getline(file, input);//todo parse first
 
-
-	while (getline(file, line))//parse input
-	{
-		// std::cout << "input: " << line << std::endl;
-		std::string date = line.substr(0, 10);
-		std::cout << "date: " << date << std::endl;
-		//! LOWERBOUND ISNT WORKING CORRECTLY
-		std::map<std::string, double>::iterator pos = db.lower_bound(date);//!doesnt work how i wanted it
-		std::cout << "data in db: " << pos->first << std::endl;
-		//TODO now calculate the value difference and print it
-	}
-
-
+	while (getline(file, input))
+		if (!processLine(db, input))
+			continue ;
 }
-
-
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
+//!NOW PUT IT IN CLASSES!!!
 int main(int ac, char **av)
 {
 	if (ac != 2) {
-		std::cout << "Invalid number of arguments" << std::endl;
+		std::cout << "Error: could not open file." << std::endl;
 		return 1;
 	}
-	//*save db
-	std::map<std::string, double> db;
-	try { //stupid shit
-		db = saveDb();
-		
-		processData(db, av);
+
+	try {
+		BitcoinExchange btc;
+		btc.process(av[1]);
 	} catch (std::exception &e) {
 		std::cerr << e.what();
 		return 1;
 	}
 
+	// std::map<std::string, double> db;
+	// try { //stupid shit
+	// 	db = saveDb();
 
+	// 	processData(db, av);
+	// } catch (std::exception &e) {
+	// 	std::cerr << e.what();
+	// 	return 1;
+	// }
 
-
-	// BitcoinExchange exchange;
-	// exchange.execute(argv[1]);
 	return 0;
 }
