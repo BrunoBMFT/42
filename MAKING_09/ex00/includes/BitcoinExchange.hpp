10cfaac19c54fa3b153ce9db2f1fb47f1aab6bab
Bruno <bmmc136@gmail.com>
2025-09-23 04:36:47 +0100

    backup

--- MAKING_09/ex00/includes/BitcoinExchange.hpp
+++ MAKING_09/ex00/includes/BitcoinExchange.hpp
@@ -2,84 +2,28 @@
 # define BITCOINEXCHANGE_HPP
 # include <iostream>
 # include <map>
-# include <fstream>//for file
-# include <stdlib.h>//for atof
+# include <fstream>
+# include <stdlib.h>
 
 class BitcoinExchange
 {
 	private:
 		std::map<std::string, double> _db;
 
-		void	processLine(std::string input)
-		{
-			if (input.length() < 12)//stupid fix
-				throw BadInput(input);
-			std::string date = input.substr(0, 10);
-			double num = atof(input.substr(12).c_str());
-			if (num < 0)
-				throw NumberNotPositive();
-			else if (num > 1000)
-				throw NumberTooLarge();
-			
-			//todo use auto?
-			std::map<std::string, double>::iterator pos = _db.lower_bound(date);
-			if (pos->first != date && pos != _db.begin())//this should be safe enough, right?
-				--pos;
-			//*calculate (make func for this)
-			double calcValue = pos->second * num;
-			std::cout << date << " => " << num << " = " << calcValue << std::endl;
-		}
+		void		processLine(std::string input);
+		std::string	getDate(std::string input);
 
 	public:
-		BitcoinExchange()
-		{
-			std::ifstream file("data.csv");
-			if (!file.is_open())
-				throw FileOpenFail();
+		//*CONSTRUCTORS
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& oth);
+		BitcoinExchange operator=(const BitcoinExchange& oth);
+		~BitcoinExchange();
 
-			std::string line;
-			getline(file, line);//todo skip, dont forget to parse this
-			while (getline(file, line))
-			{
-				std::string::size_type sep = line.find(",");//eww
-				std::string date = line.substr(0, sep);
-				std::string value = line.substr(sep + 1);//wtf
-				_db[date] = atof(value.c_str());//shit
-			}
-		}
+		//*FUNCTIONS
+		void execute(char *input); 
 
-		BitcoinExchange(const BitcoinExchange& oth) {
-			*this = oth;
-		}
-
-		BitcoinExchange operator=(const BitcoinExchange& oth) {
-			//if !=
-			_db = oth._db;
-			return *this;
-		}
-
-		~BitcoinExchange() {}
-
-		void execute(char *av) {
-			std::ifstream file(av);
-			if (!file.is_open())
-				throw std::runtime_error("Error: could not open file.\n");
-
-			std::string input;
-			getline(file, input);//todo parse first
-
-			while (getline(file, input)) {
-				try {
-					processLine(input);
-				} catch (std::exception &e) {
-					std::cerr << e.what() << std::endl;
-				}
-			}
-		}
-
-		
-
-		//exceptions
+		//*EXCEPTIONS
 		class FileOpenFail : public std::exception
 		{
 			public:
--- MAKING_09/ex00/srcs/main.cpp
+++ MAKING_09/ex00/srcs/main.cpp
@@ -1,28 +1,10 @@
 #include "../includes/BitcoinExchange.hpp"
-//todo create exceptions for everything
-
-/*
-	while (input line)
-	{
-		try
-		{
-			parse date
-			process data
-			print data
-		}
-		catch
-		{
-			whatever
-		}
-	}
-
-*/
 
 int main(int ac, char **av)
 {
 	if (ac != 2) {
-		std::cout << "Error: could not open file." << std::endl;//change
-		return 1;
+		std::cout << "Error: could not open file." << std::endl;
+		return (1);
 	}
 
 	try {
@@ -30,18 +12,8 @@ int main(int ac, char **av)
 		btc.execute(av[1]);
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
-		return 1;
+		return (1);
 	}
 
-	// std::map<std::string, double> db;
-	// try { //stupid shit
-	// 	db = saveDb();
-
-	// 	processData(db, av);
-	// } catch (std::exception &e) {
-	// 	std::cerr << e.what();
-	// 	return 1;
-	// }
-
-	return 0;
+	return (0);
 }
