9803cef9068157fd2075d0ac913e38248200af70
Bruno <bmmc136@gmail.com>
2025-10-10 03:54:20 +0100

    backup

--- IRC_mine/srcs/main.cpp
+++ IRC_mine/srcs/main.cpp
@@ -6,84 +6,89 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <string>
- 
-using namespace std;
- 
-int main()
+//when saying study, learn the returns, what the functions can do, whatever else needed
+
+//class for server
+
+void	createServer()
 {
-    // Create a socket
-    int listening = socket(AF_INET, SOCK_STREAM, 0);
-    if (listening == -1)
-    {
-        cerr << "Can't create a socket! Quitting" << endl;
-        return -1;
-    }
- 
-    // Bind the ip address and port to a socket
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(54000);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
- 
-    bind(listening, (sockaddr*)&hint, sizeof(hint));
- 
-    // Tell Winsock the socket is for listening
-    listen(listening, SOMAXCONN);
- 
-    // Wait for a connection
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
- 
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
- 
-    char host[NI_MAXHOST];      // Client's remote name
-    char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
- 
-    memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-    memset(service, 0, NI_MAXSERV);
- 
-    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-    {
-        cout << host << " connected on port " << service << endl;
-    }
-    else
-    {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout << host << " connected on port " << ntohs(client.sin_port) << endl;
-    }
- 
-    // Close listening socket
-    close(listening);
- 
-    // While loop: accept and echo message back to client
-    char buf[4096];
- 
-    while (true)
-    {
-        memset(buf, 0, 4096);
- 
-        // Wait for client to send data
-        int bytesReceived = recv(clientSocket, buf, 4096, 0);
-        if (bytesReceived == -1)
-        {
-            cerr << "Error in recv(). Quitting" << endl;
-            break;
-        }
- 
-        if (bytesReceived == 0)
-        {
-            cout << "Client disconnected " << endl;
-            break;
-        }
- 
-        cout << string(buf, 0, bytesReceived) << endl;
- 
-        // Echo message back to client
-        send(clientSocket, buf, bytesReceived + 1, 0);
-    }
- 
-    // Close the socket
-    close(clientSocket);
- 
-    return 0;
+
+	//create a socket
+	int listening = socket(AF_INET, SOCK_STREAM, 0);//*study socket, sockstream, AF_INET is IPv4
+	if (listening == -1)
+		throw (std::runtime_error("Cant create socket"));
+	
+	//bind the socket to IP / port
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);//study htons()
+	inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);//study inet_pton()
+
+	if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == -1)
+		throw (std::runtime_error("Cant bind to IP/port"));
+
+	//mark the socket for listening in
+	if (listen(listening, SOMAXCONN) == -1)//study listen()
+		throw (std::runtime_error("Cant listen"));
+	
+	//accept the call
+	sockaddr_in	client;
+	socklen_t	clientSize = sizeof(client);
+	char		host[NI_MAXHOST];
+	char		svc[NI_MAXSERV];
+
+	int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);//study accept()
+	if (clientSocket == -1)
+		throw (std::runtime_error("Problem with client connecting"));
+
+	//close the listening socket
+	close(listening);//why close the listening?
+	memset(host, 0, NI_MAXHOST);//cleans whatever garbage data was in host before (doesnt feel needed but sure)
+	memset(svc, 0, NI_MAXSERV);
+
+	int result = getnameinfo((sockaddr*)&client, clientSize, host, NI_MAXHOST, svc, NI_MAXSERV, 0);//study what this is for
+	//if getnameinfo fails, this is a cheat
+	if (result)
+		std::cout << host << " connected on " << svc << std::endl;
+	else{//the case where getnameinfo fails{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		std::cout << host << " connected on " << ntohs(client.sin_port) << std::endl;//study ntohs()
+		//study why do things manually like this
+	}
+
+	//while recieving message, echo the message
+
+	char	buf[4096];
+	while (1)
+	{
+		//clear buffer
+		memset(buf, 0, 4096);
+		//wait for a message
+		int bytesRecv = recv(clientSocket, buf, 4096, 0);//study recv()
+		if (bytesRecv == -1)
+			throw (std::runtime_error("There was a connection issue"));
+		if (bytesRecv == 0) {
+			std::cout << "The client disconnected" << std::endl;
+			break ;
+		}
+		//display message
+		std::cout << "Received: " << buf/*string(buf, 0, bytesRecv)*/ << std::endl;
+
+		//resend message
+		send (clientSocket, buf, bytesRecv + 1, 0);//study send()
+	}
+
+	//close socket
+	close(clientSocket);
+}
+
+int	main()
+{
+	try {
+		createServer();
+	} catch (std::exception &e) {
+		std::cerr << "Exception caught! " << e.what() << std::endl;
+	}
+
+	return 0;
 }
