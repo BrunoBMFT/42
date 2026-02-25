3cf8e352ad0047ab15269de469921250ac1bbcc8
BrunoBMFT <bmmc136@gmail.com>
2026-02-25 05:43:14 +0000

    backup

--- Grademe-edu/rendu/mini_serv/mini_serv.c
+++ Grademe-edu/rendu/mini_serv/mini_serv.c
@@ -1,39 +1,36 @@
 #include <string.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/select.h>
 #include <netinet/in.h>
 
 typedef struct s_client {
-	int id;
-	char *m;
+    int id;
+    char *m;
 } t_client;
 
-//fatal
-void fatal() {
-	write(2, "Fatal error\n", 12);
-	exit(1);
+void fatal(void) {
+    write(2, "Fatal error\n", 12);
+    exit(1);
 }
 
-//broadcast
-void broadcast(int fd, int maxfd, fd_set write_set, char *m) {
-	for (int i = 0; i <= maxfd; i++) {
-		if (FD_ISSET(i, &write_set) && fd != i)
-			if (send(i, m, strlen(m), 0) == -1) fatal();
-	}
+void broadcast(int fd, int max_fd, fd_set write_set, char *m) {
+    for (int i = 0; i <= max_fd; i++) {
+        if (FD_ISSET(i, &write_set) && i != fd)
+            send (i, m, strlen(m), 0);
+    }
 }
 
-//clean
-void clean(int maxfd, t_client *cls, fd_set active_set) {
-	for (int i = 0; i <= maxfd; i++) {
-		if (FD_ISSET(i, &active_set)) {
-			FD_CLR(i, &active_set);
-			close (i);
-		}
-		if (cls[i].m)
-			free (cls[i].m);
-	}
+void clean(int max_fd, t_client *cls, fd_set active_set) {
+    for (int i = 0; i <= max_fd; i++) {
+        if (FD_ISSET(i, &active_set)) {
+            FD_CLR(i, &active_set);
+            close(i);
+        }
+        if (cls[i].m)
+            free(cls[i].m);
+    }
 }
 
 int extract_message(char **buf, char **msg)
@@ -50,7 +47,8 @@ int extract_message(char **buf, char **msg)
 		if ((*buf)[i] == '\n')
 		{
 			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
-			if (newbuf == 0) fatal();
+			if (newbuf == 0)
+				fatal();
 			strcpy(newbuf, *buf + i + 1);
 			*msg = *buf;
 			(*msg)[i + 1] = 0;
@@ -72,7 +70,8 @@ char *str_join(char *buf, char *add)
 	else
 		len = strlen(buf);
 	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
-	if (newbuf == 0) fatal();
+	if (newbuf == 0)
+		fatal();
 	newbuf[0] = 0;
 	if (buf != 0)
 		strcat(newbuf, buf);
@@ -83,59 +82,90 @@ char *str_join(char *buf, char *add)
 
 
 int main(int ac, char **av) {
-	if (ac != 2) {
-		write(2, "whateveraaaaaaaaaaaaaaaaaa", 26);
-		return 1;
-	}
-	int sockfd, i, len, maxfd, id;
-	struct sockaddr_in servaddr, cli; 
+    if (ac != 2) {
+        write (2, "Wrong number of arguments\n", 26);
+        exit(1);
+    }
+	int sockfd, len, i, max_fd, id;
+	struct sockaddr_in servaddr, cli;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) fatal();
 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(atoi(av[1])); 
-  
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433);
+	servaddr.sin_port = htons(atoi(av[1]));
+
 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) fatal();
 	if (listen(sockfd, 10) != 0) fatal();
-
-
+	
 	len = sizeof(cli);
-	int BSIZE = 1024, MSIZE = 64, CSIZE = 16*4096;
-	t_client cls[CSIZE];
-	fd_set active_set, write_set, read_set;
-	FD_ZERO(&active_set);
-	FD_SET(sockfd, &active_set);
-	maxfd = sockfd;
-	id = 0;
+    const int BSIZE = 1024, MSIZE = 64, CSIZE = 16*4096;
+    t_client  cls[CSIZE];
+    fd_set active_set, read_set, write_set;
+    FD_ZERO(&active_set);
+    FD_SET(sockfd, &active_set);
+    max_fd = sockfd;
+    id = 0;
 
-	while (1) {
-		read_set = write_set = active_set;
-		//if select fatal()
-		if (select(maxfd + 1, &read_set, &write_set, NULL, NULL) == -1) {
-			clean(maxfd, cls, active_set);
-			fatal();
-		}
-		if (FD_ISSET(sockfd, &read_set)) {
-			i = accept(sockfd, (struct sockaddr *)&cli, &len);
-			if (i >= 0) {
-				cls[i].id = id++;
-				cls[i].m = NULL;
-				FD_SET(i, &active_set);
-				if (i > maxfd) maxfd = i;
-				char m[MSIZE];
-				bzero(m, MSIZE);
-				sprintf(m, "server: client %d just arrived\n", cls[i].id);
-				broadcast(i, maxfd, write_set, m);
-			}
-		}
-		else {
-			for (int i = 0; i <= maxfd; i++) {
-				if ()
-			}
-		}
-	}
+    while (1) {
+        read_set = write_set = active_set;
+        int active = select(max_fd + 1, &read_set, &write_set, NULL, NULL);
+        if (active == -1) {
+            clean(max_fd, cls, active_set);
+            fatal();
+        }
 
-
+        if (FD_ISSET(sockfd, &read_set)) {
+            i = accept(sockfd, (struct sockaddr *)&cli, &len);
+            if (i >= 0) {
+                cls[i].id = id++;
+                cls[i].m = NULL;
+                FD_SET(i, &active_set);
+                if (i > max_fd) max_fd = i;
+                char m[MSIZE];
+                bzero(m, MSIZE);
+                sprintf(m, "server: client %d just arrived\n", cls[i].id);
+                broadcast(i, max_fd, write_set, m);
+            }
+        }
+        else {
+            for (i = 0; i <= max_fd; i++) {
+                if (FD_ISSET(i, &read_set)) {
+                    char buff[BSIZE];
+                    bzero(buff, BSIZE);
+                    if (recv(i, buff, BSIZE - 1, 0) <= 0) {
+                        char m[MSIZE];
+                        bzero(m, MSIZE);
+                        sprintf(m, "server: client %d just left\n", cls[i].id);
+                        broadcast(i, max_fd, write_set, m);
+                        FD_CLR(i, &active_set);
+                        close(i);
+                        if (cls[i].m) {
+                            free(cls[i].m);
+                            cls[i].m = NULL;
+                        }
+                    }
+                    else {
+                        char *line = NULL;
+                        cls[i].m = str_join(cls[i].m, buff);
+                        while(extract_message(&(cls[i].m), &line)) {
+                            char m[MSIZE + strlen(line)];
+                            bzero(m, MSIZE + strlen(line));
+                            sprintf(m, "client %d: %s", cls[i].id, line);
+                            broadcast(i, max_fd, write_set, m);
+                            free(line);
+                            line = NULL;
+                        }
+                        if (cls[i].m[0] == '\0') {
+                            free(cls[i].m);
+                            cls[i].m = NULL;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    clean(max_fd, cls, active_set);
+    return (0);
 }
