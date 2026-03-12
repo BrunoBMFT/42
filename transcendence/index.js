2ffb1ed9fd8eaf3208c0defb17695df9afc9f02e
BrunoBMFT <bmmc136@gmail.com>
2026-03-12 15:22:02 +0000

    backup

--- transcendence/index.js
+++ transcendence/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/w', (req, res) => {
-	res.send("ahahaha");
+app.get('/api/aaa', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello retard')
+	// res.send("ahahaha");
 });
 
 app.listen(3000, () => {
